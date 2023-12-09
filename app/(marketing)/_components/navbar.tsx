259f7224b8fc1fee248a913b9d8e2459938a45fe
Hidetoshi Hibino <hibinozzzz@gmail.com>
2023-12-09 09:53:49 +0900

    NavbarへのLoadingコンポーネントの実装

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,11 +1,17 @@
 'use client'
 
+import { SignInButton, UserButton } from '@clerk/clerk-react'
+import { useConvexAuth } from 'convex/react'
+import Link from 'next/link'
 import { Logo } from './logo'
 import { ModeToggle } from '@/components/mode-toggle'
+import { Spinner } from '@/components/spinner'
+import { Button } from '@/components/ui/button'
 import { useScrollTop } from '@/hooks/useScrollTop'
 import { cn } from '@/lib/utils'
 
 export const Navbar = () => {
+  const { isLoading, isAuthenticated } = useConvexAuth()
   const scrolled = useScrollTop()
   return (
     <div
@@ -16,6 +22,27 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Jotion free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Jotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
