a235a8a67b3c3ee1744380ba0c95edc1f4806ec9
Hidetoshi Hibino <hibinozzzz@gmail.com>
2023-12-20 19:03:53 +0900

    layout にedgestoreのproviderを実装

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from 'sonner'
 import { ConvexClientProvider } from '@/components/proveiders/convex-provider'
 import { ModalProvider } from '@/components/proveiders/modal-provider'
 import { ThemeProvider } from '@/components/proveiders/theme-provider'
+import { EdgeStoreProvider } from '@/lib/edgestore'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -36,17 +37,19 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ConvexClientProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-            storageKey="jotion-theme"
-          >
-            <Toaster position="bottom-center" />
-            <ModalProvider />
-            {children}
-          </ThemeProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+              storageKey="jotion-theme"
+            >
+              <Toaster position="bottom-center" />
+              <ModalProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
         </ConvexClientProvider>
       </body>
     </html>
