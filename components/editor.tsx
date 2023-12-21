6b4afb36a9fa03a04831af045cf6e360578cc4ca
Hidetoshi Hibino <hibinozzzz@gmail.com>
2023-12-21 10:37:02 +0900

    画像のアップロード

--- components/editor.tsx
+++ components/editor.tsx
@@ -4,6 +4,7 @@ import { BlockNoteEditor, PartialBlock } from '@blocknote/core'
 import { BlockNoteView, useBlockNote } from '@blocknote/react'
 import '@blocknote/core/style.css'
 import { useTheme } from 'next-themes'
+import { useEdgeStore } from '@/lib/edgestore'
 
 interface EditorProps {
   onChange: (value: string) => void
@@ -12,6 +13,15 @@ interface EditorProps {
 
 const Editor = ({ onChange, initialContent }: EditorProps) => {
   const { resolvedTheme } = useTheme()
+  const { edgestore } = useEdgeStore()
+
+  const handleUpload = async (file: File) => {
+    const response = await edgestore.publicFiles.upload({
+      file,
+    })
+
+    return response.url
+  }
 
   const editor: BlockNoteEditor = useBlockNote({
     initialContent: initialContent
@@ -20,6 +30,7 @@ const Editor = ({ onChange, initialContent }: EditorProps) => {
     onEditorContentChange: (editor) => {
       onChange(JSON.stringify(editor.topLevelBlocks, null, 2))
     },
+    uploadFile: handleUpload,
   })
 
   return (
