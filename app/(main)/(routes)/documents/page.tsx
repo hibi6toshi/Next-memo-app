1d143ee36e5782f25330e73a874f6e303d07bae6
Hidetoshi Hibino <hibinozzzz@gmail.com>
2023-12-21 10:47:02 +0900

    新規作成後にそのページへジャンプ

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -4,16 +4,20 @@ import { useUser } from '@clerk/clerk-react'
 import { useMutation } from 'convex/react'
 import { PlusCircle } from 'lucide-react'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import { toast } from 'sonner'
 import { Button } from '@/components/ui/button'
 import { api } from '@/convex/_generated/api'
 
 const DocumentsPage = () => {
+  const router = useRouter()
   const { user } = useUser()
   const create = useMutation(api.documents.create)
 
   const onClick = () => {
-    const promise = create({ title: 'Untitled' })
+    const promise = create({ title: 'Untitled' }).then((documentId) =>
+      router.push(`/documents/${documentId}`),
+    )
 
     toast.promise(promise, {
       loading: 'Creating a new note...',
